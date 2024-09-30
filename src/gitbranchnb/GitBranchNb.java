1974e766805ddb551db8f4ff60c3955c68dac72b
PalkAlex23 <alexpytmusic@gmail.com>
2024-09-30 13:23:25 +0200

    int fv()

--- src/gitbranchnb/GitBranchNb.java
+++ src/gitbranchnb/GitBranchNb.java
@@ -2,11 +2,11 @@ package gitbranchnb;
 public class GitBranchNb {
 
     public static void main(String[] args) {
-        int i = fv();
+        GitBranchNb obj = new GitBranchNb();
+        int i = obj.fv();
     }
 
-    private static int fv() {
+    private int fv() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
 }
